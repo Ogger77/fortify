3175b9db42d555bfd427106e51f7be840c350a21
Ogger77 <vudinhcotdien@outlook.com>
2019-04-15 14:08:24 -0400

    finish recipe

--- src/js/index.js
+++ src/js/index.js
@@ -102,5 +102,20 @@ const controlRecipe = async () => {
 
 // window.addEventListener('hashchange', controlRecipe);
 // window.addEventListener('load', controlRecipe);
-
 ['hashchange', 'load'].forEach(event => window.addEventListener(event, controlRecipe));
+
+//Handling recipe button click
+elements.recipe.addEventListener('click', e => {
+    //select the btn-decrease and all of its child
+    if(e.target.matches('.btn-decrease, .btn-decrease *')){
+        //Decrease button clicked
+        if(state.recipe.servings > 1)
+            state.recipe.updateServing('dec');
+            recipeView.updateServingsIngredients(state.recipe);
+    }else if(e.target.matches('.btn-increase, .btn-increase *')){
+        //increase button clicked
+        state.recipe.updateServing('inc');
+        recipeView.updateServingsIngredients(state.recipe);
+    }
+    console.log(state.recipe);
+})
